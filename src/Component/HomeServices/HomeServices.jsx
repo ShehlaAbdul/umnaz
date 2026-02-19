be5da96393661c62c9813cf9af14837c1fc2de09
Shahla <shahlaabdullayevaa@gmail.com>
2026-02-19 16:20:17 +0400

    cards are created

---
+++ src/Component/HomeServices/HomeServices.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import "./Style.scss";
+import MainBtn from "../MainBtn/MainBtn";
+
+function HomeServices() {
+  const details = [
+    {
+      id: 1,
+      title: "Ümumi xidmət bloku",
+      description:
+        "Hər şey bir ünvanda. Eskiz layihələndirmə, Geoloji həllər və konstruksiya hesablamaları, İnteryer və eksteryer dizayn xidmətləri.",
+    },
+    {
+      id: 2,
+      title: "Restoranlar üçün",
+      description:
+        "Restoranlar üçün interyer və eksteryer dizayn xidməti. Fəaliyyətinizə uyğun, estetik və funksional məkan həlləri təqdim edirik.",
+    },
+    {
+      id: 3,
+      title: "Tikintiyə icazə üçün layihələr",
+      description:
+        "Tikintiyə icazənin alınması üçün Həyət və bağ evlərinin layihələrinin hazırlanması.",
+    },
+    {
+      id: 4,
+      title: "Mağazalar üçün",
+      description:
+        "Mağazalar üçün interyer və eksteryer dizayn xidməti. Brendinizə uyğun müasir və cəlbedici dizayn konseptləri.",
+    },
+  ];
+
+  return (
+    <section id="home-services">
+      <div className="home-services container-fluid g-0 m-0 p-0">
+        {/* HEAD */}
+        <div className="head-side">
+          <p>Xidmətlərimiz</p>
+          <h1>Xidmətlər</h1>
+        </div>
+
+        {/* CONTENT */}
+        <div className="content row g-4">
+          {details.map((item) => (
+            <div className="col-12 col-md-6" key={item.id}>
+              <div className="card col-8">
+                <div className="card-id">/{item.id}</div>
+
+                <div className="card-content">
+                  <h5>{item.title}</h5>
+                  <p>{item.description}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {/* BUTTON */}
+        <MainBtn />
+      </div>
+    </section>
+  );
+}
+
+export default HomeServices;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import "./Style.scss";
 import HomeContactForm from '../../Component/HomeContactForm/HomeContactForm';
 import HomeEstetics from '../../Component/HomeEstetics/HomeEstetics';
+import HomeServices from '../../Component/HomeServices/HomeServices';
 
 function Home() {
   return (
     <>
+      <HomeServices/>
       <HomeEstetics/>
       <HomeContactForm/>
     </>

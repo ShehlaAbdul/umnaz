550dc5ef12e76ca662b7bd0d36019de490661a77
Shahla <shahlaabdullayevaa@gmail.com>
2026-02-23 01:15:15 +0400

    partners page is ready

--- src/Pages/Partners/Partners.jsx
+++ src/Pages/Partners/Partners.jsx
@@ -2,18 +2,23 @@ import React from "react";
 import "./Style.scss";
 import HeroSection from "../../Component/HeroSection/HeroSection";
 import bgImage from "../../assets/images/HeroSection.webp";
+import HomePartners from "../../Component/HomePartners/HomePartners";
 
 function Partners() {
   return (
     <>
       <HeroSection title={"Tərəfdaşlar"} bgImage={bgImage} />
       <section id="partners-page">
-        <div className="partners-page-top container-fluid g-0 d-flex justify-content-center ">
-          <div className="overlay">1</div>
-          <p>
+        <div className="partners-page container-fluid g-0 d-flex justify-content-center ">
+          <p className="top">
             Peşəkar iş ortaqlarımız və strateji partnyorumuzla birlikdə müasir
             memarlıq yanaşmalarına əsaslanan layihələr həyata keçiririk.
           </p>
+          <div className="overlay"></div>
+        </div>
+        <div className="our-partners">
+                  <p className="partners-title ">İş ortaqlarımız</p>
+                  <HomePartners/>
         </div>
       </section>
     </>
